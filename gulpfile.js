fe1e2b0086c0fca54f56d88563f09c090ff3c268
Alexander <strannik11.92@gmail.com>
2016-09-25 15:47:10 +0300

    The initial assembly of styles and HTML

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,22 @@
+"use strict";
+
 //Подключаем gulp и плагины
-var gulp = require('gulp'),
-	autoprefixer = require('gulp-autoprefixer'),
-	browserSync = require("browser-sync"),
-	cssmin = require('gulp-minify-css'),
-	del = require('del'),
-	imagemin = require('gulp-imagemin'),
-	plumber = require('gulp-plumber'),
-	pug = require('gulp-pug'),
-	rigger = require('gulp-rigger'),
-	sass = require('gulp-sass'),
-	sourcemaps = require('gulp-sourcemaps'),
-	uglify = require('gulp-uglify'),
-	reload = browserSync.reload;
+const gulp = require('gulp');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require("browser-sync");
+const cssmin = require('gulp-minify-css');
+const del = require('del');
+const imagemin = require('gulp-imagemin');
+const plumber = require('gulp-plumber');
+const pug = require('gulp-pug');
+const rigger = require('gulp-rigger');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
+const reload = browserSync.reload;
+const gulpIf = require('gulp-if');
+
+const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == "development";
 
 //Создаем перемнные где прописаны все пути
 var path = {
@@ -23,7 +28,7 @@ var path = {
 		fonts: 'build/fonts/'
 	},
 	src: {						//Указываем пути откуда брать исходники
-		html: 'src/*.pug',
+		html: 'src/index.pug',
 		js: 'src/js/main.js',
 		style: 'src/style/main.scss',
 		img: 'src/img/**/*.*',
@@ -52,94 +57,99 @@ var config = {
 
 //Создаем задание собрать HTML
 gulp.task('html:build', function () {
-	gulp.src(path.src.html)
-			.pipe(plumber())
-			.pipe(rigger())
-			.pipe(pug({
-				pretty: true
-			}))
-			.pipe(gulp.dest(path.build.html))
-			.pipe(reload({stream: true}));
+	return gulp.src(path.src.html)
+		.pipe(plumber())
+		.pipe(rigger())
+		.pipe(pug({
+			pretty: true
+		}))		
+		.pipe(gulp.dest(path.build.html))
+//		.pipe(reload({stream: true}));
 });
 
 //Создаем задание собрать JavaScript
-gulp.task('js:build', function () {
-	gulp.src(path.src.js)
-			.pipe(plumber())
-			.pipe(rigger())
-			.pipe(sourcemaps.init())
-			 // .pipe(uglify())
-			.pipe(sourcemaps.write())
-			.pipe(gulp.dest(path.build.js))
-			.pipe(reload({stream: true}));
-});
+// gulp.task('js:build', function () {
+// 	gulp.src(path.src.js)
+// 			.pipe(plumber())
+// 			.pipe(rigger())
+// 			.pipe(sourcemaps.init())
+// 			 // .pipe(uglify())
+// 			.pipe(sourcemaps.write())
+// 			.pipe(gulp.dest(path.build.js))
+// 			.pipe(reload({stream: true}));
+// });
 
 //Создаем задание собрать SCSS
 gulp.task('style:build', function () {
-	gulp.src(path.src.style)
-			.pipe(plumber())
-			.pipe(sourcemaps.init())
-			.pipe(sass())
-			.pipe(autoprefixer())
-			// .pipe(cssmin())
-			.pipe(sourcemaps.write())
-			.pipe(gulp.dest(path.build.css))
-			.pipe(reload({stream: true}));
+	return gulp.src(path.src.style)
+		.pipe(plumber())
+		.pipe(gulpIf(isDevelopment, sourcemaps.init()))
+		.pipe(sass())
+		.pipe(autoprefixer())
+		// .pipe(cssmin())
+		.pipe(gulpIf(isDevelopment, sourcemaps.write()))
+		.pipe(gulp.dest(path.build.css))
+		.pipe(reload({stream: true}));
 })
 
-//Создаем задание собрать картинки
-gulp.task('image:build', function () {
-	gulp.src(path.src.img)
-			.pipe(plumber())
-			//.pipe(imagemin ())
-			.pipe(gulp.dest(path.build.img))
-			.pipe(reload({stream: true}));
-});
+// //Создаем задание собрать картинки
+// gulp.task('image:build', function () {
+// 	gulp.src(path.src.img)
+// 			.pipe(plumber())
+// 			//.pipe(imagemin ())
+// 			.pipe(gulp.dest(path.build.img))
+// 			.pipe(reload({stream: true}));
+// });
 
-//Создаем задание собрать шрифты
-gulp.task('fonts:build', function() {
-	gulp.src(path.src.fonts)
-			.pipe(plumber())
-			.pipe(gulp.dest(path.build.fonts))
-});
+// //Создаем задание собрать шрифты
+// gulp.task('fonts:build', function() {
+// 	gulp.src(path.src.fonts)
+// 			.pipe(plumber())
+// 			.pipe(gulp.dest(path.build.fonts))
+// });
 
-//Создаем задание для всей сборки
-gulp.task('build', [
-	'html:build',
-	'js:build',
-	'style:build',
-	'fonts:build',
-	'image:build'
-]);
+// //Создаем задание для всей сборки
+// gulp.task('build', [
+// 	'html:build',
+// 	'js:build',
+// 	'style:build',
+// 	'fonts:build',
+// 	'image:build'
+// ]);
 
-//Создаем задание для автоматической сборки при изменении файла
-gulp.task('watch', function(){
-	gulp.watch([path.watch.html], function(event, cb) {
-		gulp.start('html:build');
-	});
-	gulp.watch([path.watch.style], function(event, cb) {
-		gulp.start('style:build');
-	});
-	gulp.watch([path.watch.js], function(event, cb) {
-		gulp.start('js:build');
-	});
-	gulp.watch([path.watch.img], function(event, cb) {
-		gulp.start('image:build');
-	});
-	gulp.watch([path.watch.fonts], function(event, cb) {
-		gulp.start('fonts:build');
-	});
-});
 
-//Создаем задание для запуска Dev сервера
-gulp.task('webserver', function () {
-	browserSync(config);
-});
+// //Создаем задание для автоматической сборки при изменении файла
+// gulp.task('watch', function(){
+// 	gulp.watch([path.watch.html], function(event, cb) {
+// 		gulp.start('html:build');
+// 	});
+// 	gulp.watch([path.watch.style], function(event, cb) {
+// 		gulp.start('style:build');
+// 	});
+// 	gulp.watch([path.watch.js], function(event, cb) {
+// 		gulp.start('js:build');
+// 	});
+// 	gulp.watch([path.watch.img], function(event, cb) {
+// 		gulp.start('image:build');
+// 	});
+// 	gulp.watch([path.watch.fonts], function(event, cb) {
+// 		gulp.start('fonts:build');
+// 	});
+// });
+
+// //Создаем задание для запуска Dev сервера
+// gulp.task('webserver', function () {
+// 	browserSync(config);
+// });
 
 //Создаем задание для очистки папки build
 gulp.task('clean', function (cb) {
-	del(path.clean, cb);
+	return del(path.clean, cb);
 })
 
-//Создаем задание для запуска всей сборки, Dev сервера и gulp-watch
-gulp.task('default', ['build', 'webserver', 'watch']);
+gulp.task('build', gulp.series(
+	'clean',
+	gulp.parallel('html:build', 'style:build')));
+
+// //Создаем задание для запуска всей сборки, Dev сервера и gulp-watch
+// gulp.task('default', ['build', 'webserver', 'watch']);
