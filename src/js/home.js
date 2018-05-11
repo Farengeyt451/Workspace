95d4b5499747ca71b102b2c8511ef9869e56faef
Alexander <strannik11.92@gmail.com>
2018-05-11 18:10:26 +0300

    Add fun to close popup search

--- src/js/home.js
+++ src/js/home.js
@@ -1,13 +1,26 @@
 "use strict";
 
-
 const hamburger = $(".burger-menu"),
-			popupBar = $("#popupSearch");
+			popupTrigger = $("#popupSearch"),
+			popupSearh = $(".popup-text"),
+			popupSearhWrapper = $(".popup-search");
 
-popupBar.on("click", function(){
-	$(".popuptext").toggleClass("show");
+// Open popup search bar
+popupTrigger.on("click", function(){
+	popupSearh.toggleClass("show");
 });
 
+// Close popup when click outside search bar
+$(document).on("click", function(event) {
+	if ((!$(event.target).closest(popupTrigger).length)) {
+		if (!$(event.target).closest(popupSearhWrapper).length) {
+				popupSearh.removeClass("show");
+		}
+	}
+});
+
+// Trigger hamburger menu icon animation
 hamburger.on("click", function(){
 	hamburger.toggleClass("hamburger-active");
 });
+
